068e82122bbb7050bcd619b3fc2e8d7e1a967509
xingoxu <xingoxu@gmail.com>
2019-06-26 18:41:15 +0900

    init 153

---
+++ 153.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    }
+};
+
+int main()
+{
+  vector<int> x{3, 4, 5, 1, 2};
+  cout << Solution().findMin(x) << endl;
+  return 0;
+}
