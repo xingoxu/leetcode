53130676c048c7a9c13f72f8c8ea1fae4c924d12
xingoxu <xingoxu@gmail.com>
2019-08-11 22:27:25 +0900

    226

---
+++ 226.cpp
@@ -0,0 +1,144 @@
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Invert Binary Tree.
+// Memory Usage: 9.1 MB, less than 96.36% of C++ online submissions for Invert Binary Tree.
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <sstream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+                    return !isspace(ch);
+                }));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+                    return !isspace(ch);
+                })
+                    .base(),
+                input.end());
+}
+
+TreeNode *stringToTreeNode(string input)
+{
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size())
+    {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode *root = new TreeNode(stoi(item));
+    queue<TreeNode *> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true)
+    {
+        TreeNode *node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ','))
+        {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null")
+        {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ','))
+        {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null")
+        {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string treeNodeToString(TreeNode *root)
+{
+    if (root == nullptr)
+    {
+        return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode *node = q.front();
+        q.pop();
+
+        if (node == nullptr)
+        {
+            output += "null, ";
+            continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+class Solution
+{
+public:
+  TreeNode *invertTree(TreeNode *root)
+  {
+    if (!root)
+      return root;
+    if (root->left)
+    {
+      invertTree(root->left);
+    }
+    if (root->right)
+    {
+      invertTree(root->right);
+    }
+    auto tmp = root->left;
+    root->left = root->right;
+    root->right = tmp;
+    return root;
+  }
+};
+
+int main()
+{
+  cout << treeNodeToString(Solution().invertTree(stringToTreeNode("[4,2,7,1,3,6,9]"))) << endl;
+
+  return 0;
+}
