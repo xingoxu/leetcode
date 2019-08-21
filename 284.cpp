afbb6ebc6d6241dd7823e54c9a0d52f6240e4927
xingoxu <xingoxu@gmail.com>
2019-08-21 21:38:17 +0900

    284

---
+++ 284.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Iterator
+{
+  struct Data;
+  Data *data;
+
+public:
+  Iterator(const vector<int> &nums);
+  Iterator(const Iterator &iter);
+  virtual ~Iterator();
+  // Returns the next element in the iteration.
+  int next();
+  // Returns true if the iteration has more elements.
+  bool hasNext() const;
+};
+class PeekingIterator : public Iterator
+{
+public:
+  PeekingIterator(const vector<int> &nums) : Iterator(nums)
+  {
+  }
+
+  int peek()
+  {
+    return Iterator(*this).next();
+  }
+
+  int next()
+  {
+    return Iterator::next();
+  }
+
+  bool hasNext() const
+  {
+    return Iterator::hasNext();
+  }
+};
+
+int main()
+{
+  return 0;
+}
