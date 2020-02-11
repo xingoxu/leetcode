678ca268bc3ffa65b79c5707e28105948efe5462
xingoxu <xingoxu@gmail.com>
2020-02-12 08:48:42 +0900

    508 & 513

---
+++ 513.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+void trimLeftTrailingSpaces(string &input)
+{
+  input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+                return !isspace(ch);
+              }));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+  input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+                return !isspace(ch);
+              })
+                  .base(),
+              input.end());
+}
+
+TreeNode *stringToTreeNode(string input)
+{
+  trimLeftTrailingSpaces(input);
+  trimRightTrailingSpaces(input);
+  input = input.substr(1, input.length() - 2);
+  if (!input.size())
+  {
+    return nullptr;
+  }
+
+  string item;
+  stringstream ss;
+  ss.str(input);
+
+  getline(ss, item, ',');
+  TreeNode *root = new TreeNode(stoi(item));
+  queue<TreeNode *> nodeQueue;
+  nodeQueue.push(root);
+
+  while (true)
+  {
+    TreeNode *node = nodeQueue.front();
+    nodeQueue.pop();
+
+    if (!getline(ss, item, ','))
+    {
+      break;
+    }
+
+    trimLeftTrailingSpaces(item);
+    if (item != "null")
+    {
+      int leftNumber = stoi(item);
+      node->left = new TreeNode(leftNumber);
+      nodeQueue.push(node->left);
+    }
+
+    if (!getline(ss, item, ','))
+    {
+      break;
+    }
+
+    trimLeftTrailingSpaces(item);
+    if (item != "null")
+    {
+      int rightNumber = stoi(item);
+      node->right = new TreeNode(rightNumber);
+      nodeQueue.push(node->right);
+    }
+  }
+  return root;
+}
+
+string treeNodeToString(TreeNode *root)
+{
+  if (root == nullptr)
+  {
+    return "[]";
+  }
+
+  string output = "";
+  queue<TreeNode *> q;
+  q.push(root);
+  while (!q.empty())
+  {
+    TreeNode *node = q.front();
+    q.pop();
+
+    if (node == nullptr)
+    {
+      output += "null, ";
+      continue;
+    }
+
+    output += to_string(node->val) + ", ";
+    q.push(node->left);
+    q.push(node->right);
+  }
+  return "[" + output.substr(0, output.length() - 2) + "]";
+}
+class Solution
+{
+public:
+  int findBottomLeftValue(TreeNode *root)
+  {
+    if(!root)
+      return 0;
+    queue<TreeNode *> level;
+    level.push(root);
+    auto ret = root;
+    while (!level.empty())
+    {
+      ret = level.front();
+      queue<TreeNode *> nextLevel;
+      while (!level.empty())
+      {
+        auto node = level.front();
+        level.pop();
+        if (node->left)
+          nextLevel.push(node->left);
+        if (node->right)
+          nextLevel.push(node->right);
+      }
+      level = nextLevel;
+    }
+    return ret->val;
+  }
+};
+
+int main()
+{
+  auto result = Solution().findBottomLeftValue(stringToTreeNode("[2,1,3]"));
+  cout << result << endl;
+  return 0;
+}
